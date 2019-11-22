f6b3ddc363323c631fded099e9c826a2e8f85575
David Brady <dgrbrady@gmail.com>
2019-11-22 05:58:36 -0500

    Adding sentry

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, ErrorHandler, Injectable } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -9,16 +9,31 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material.module';
 import { ScoreComponent } from './components/score/score.component';
 
+import * as Sentry from '@sentry/browser';
+
+Sentry.init({
+    dsn: 'https://b82e9042775f4941876654a4fb81d785@sentry.io/1830815'
+});
+
+@Injectable()
+export class SentryErrorHandler implements ErrorHandler {
+    constructor() {}
+    handleError(error) {
+        const eventId = Sentry.captureException(error.originalError || error);
+        Sentry.showReportDialog({ eventId });
+    }
+}
+
 @NgModule({
-  declarations: [AppComponent, PostComponent, ScoreComponent],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule,
-    BrowserAnimationsModule,
-    MaterialModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [AppComponent, PostComponent, ScoreComponent],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        HttpClientModule,
+        BrowserAnimationsModule,
+        MaterialModule
+    ],
+    providers: [{ provide: ErrorHandler, useClass: SentryErrorHandler }],
+    bootstrap: [AppComponent]
 })
 export class AppModule {}
