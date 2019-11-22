a78228b7c707bb92c53000c90f1810f6a1c1cc85
David Brady <dgrbrady@gmail.com>
2019-11-22 02:09:44 -0500

    adding slide animation to result div

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,53 +3,74 @@ import { RedditService } from './reddit.service';
 import { map } from 'rxjs/operators';
 import { zip } from 'rxjs';
 import { ScoreComponent } from './components/score/score.component';
+import { trigger, style, transition, animate } from '@angular/animations';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css'],
+    animations: [
+        trigger('slide', [
+            transition(':enter', [
+                style({ transform: 'translateY(-100%)' }),
+                animate('200ms ease-in', style({ transform: 'translateY(0%)' }))
+            ])
+        ])
+    ]
 })
 export class AppComponent implements OnInit {
-  constructor(private reddit: RedditService) {}
-  title = 'Fake-News';
-  posts: any[] = [];
-  total = 0;
-  post: { title: string; dataSet: 'real' | 'fake' };
-  correct: boolean;
-  showDescription = true;
-  @ViewChild('score', { static: true }) score: ScoreComponent;
+    constructor(private reddit: RedditService) {}
+    title = 'Fake-News';
+    posts: any[] = [];
+    total = 0;
+    post: { title: string; dataSet: 'real' | 'fake' };
+    correct: boolean;
+    showDescription = true;
+    answered = false;
+    @ViewChild('score', { static: true }) score: ScoreComponent;
 
-  ngOnInit() {
-    function resultMapper(title: string, dataSet: 'real' | 'fake') {
-      return { title, dataSet };
+    ngOnInit() {
+        function resultMapper(title: string, dataSet: 'real' | 'fake') {
+            return { title, dataSet };
+        }
+        zip(
+            this.reddit
+                .getFakeNews()
+                .pipe(
+                    map(posts =>
+                        posts.map(title => resultMapper(title, 'fake'))
+                    )
+                ),
+            this.reddit
+                .getRealNews()
+                .pipe(
+                    map(posts =>
+                        posts.map(title => resultMapper(title, 'real'))
+                    )
+                )
+        ).subscribe(posts => {
+            this.posts = posts[0].concat(posts[1]);
+            this.total = this.posts.length;
+        });
     }
-    zip(
-      this.reddit
-        .getFakeNews()
-        .pipe(map(posts => posts.map(title => resultMapper(title, 'fake')))),
-      this.reddit
-        .getRealNews()
-        .pipe(map(posts => posts.map(title => resultMapper(title, 'real'))))
-    ).subscribe(posts => {
-      this.posts = posts[0].concat(posts[1]);
-      this.total = this.posts.length;
-    });
-  }
 
-  getPost() {
-    this.correct = undefined;
-    this.showDescription = false;
-    const random = Math.floor(Math.random() * this.posts.length);
-    this.post = this.posts[random];
-    this.posts.splice(random, 1);
-  }
+    getPost() {
+        this.answered = false;
+        this.correct = undefined;
+        this.showDescription = false;
+        const random = Math.floor(Math.random() * this.posts.length);
+        this.post = this.posts[random];
+        this.posts.splice(random, 1);
+    }
 
-  guessCorrect() {
-    this.score.correct += 1;
-    this.correct = true;
-  }
+    guessCorrect() {
+        this.score.correct += 1;
+        this.correct = true;
+        this.answered = true;
+    }
 
-  guessWrong() {
-    this.correct = false;
-  }
+    guessWrong() {
+        this.correct = false;
+        this.answered = true;
+    }
 }
