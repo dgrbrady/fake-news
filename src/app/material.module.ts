5e2c7b989b062461b4f00d9788a1703bdd808939
doobie <david_bradyw@yahoo.com>
2022-01-20 14:31:54 -0500

    updated cdk and material to v8

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 
 import { MatButtonModule } from '@angular/material/button';
-import { MatCardModule } from '@angular/material';
+import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
   imports: [MatButtonModule, MatCardModule],
